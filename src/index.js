19a3ccca119f70815c67d4932297fd291a32ef36
ZumTheZazaKing <zahidi85543@gmail.com>
2021-07-29 18:06:00 +0800

    Initial state and layout of the app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,57 @@
 import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import reportWebVitals from './reportWebVitals';
+
+
+
+function Input(){
+
+  return <div className="inputContainer">
+
+    <h2>BMI Calculator</h2>
+
+    <form>
+
+      <label>Weight(kg): </label>
+      <input type="number" value="" />
+
+      <br/><br/>
+
+      <label>Height(m): </label>
+      <input type="number" value="" />
+
+      <br/><br/>
+
+      <input type="submit" value="Calculate" />
+    </form>
+
+  </div>
+
+}
+
+function Output(){
+
+  return <div>
+
+    <h3>Your BMI is:</h3>
+    <h2>Sample Text</h2>
+    <p>Sample Text</p>
+
+  </div>
+
+}
+
+
 
 function App(){
 
   return <div>
 
-    <h1>Hello</h1>
+    <Input />
+    <Output />
+
 
   </div>
 
